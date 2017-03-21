c18a87f697e953e2ea3773b44a937de5940c4bc7
Yejee Arin Choi <yej.arin.choi@gmail.com>
2017-03-21 16:42:20 -0400

    working dropdowns!

--- public/js/itenerary.js
+++ public/js/itenerary.js
@@ -1,3 +1,11 @@
 $(function listAllPlaces() {
-  $('#hotelDropDown')
+  for (var i=0; i < hotels.length; i++) {
+    $('#hotelsDropDown').append('<option>'+ hotels[i].name +'</option>')
+  }
+  for (var i=0; i < restaurants.length; i++) {
+    $('#restaurantsDropDown').append('<option>'+ restaurants[i].name +'</option>')
+  }
+  for (var i=0; i < activities.length; i++) {
+    $('#activitiesDropDown').append('<option>'+ activities[i].name +'</option>')
+  }
 })
