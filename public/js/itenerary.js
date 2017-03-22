e6ec5e15764fddf634bbece4654bdfec574972b4
Yejee Arin Choi <yej.arin.choi@gmail.com>
2017-03-22 11:29:26 -0400

    able to remove location from list

--- public/js/itenerary.js
+++ public/js/itenerary.js
@@ -1,3 +1,5 @@
+// DROPDOWN OPTIONS INTIALIZED
+
   $(function listAllPlaces() {
   for (var i=0; i < hotels.length; i++) {
     $('#hotelsDropDown').append('<option>'+ hotels[i].name +'</option>')
@@ -10,22 +12,24 @@
   }
 })
 
+// ADD ITINERARY ITEMS
+
 $("#hotelAdd").on('click', function(){
-  console.log($('#hotelsDropDown :selected'), hotels)
   $("#bookedHotel").append('<span>' + $("#hotelsDropDown :selected").text() +'</span>')
+  $('#bookedHotel').append('<button class="btn btn-xs btn-danger remove btn-circle pull-right">x</button>')
   var hotelCoordinate;
   for (var i =0; i<hotels.length; i++){
     if ( hotels[i].name === $("#hotelsDropDown :selected").text() ){
       hotelCoordinate = hotels[i].place.location
     }
   }
-  console.log(hotelCoordinate)
   drawMarker('hotel', hotelCoordinate)
 })
 
 $("#restaurantAdd").on('click', function(){
-  console.log($('#restaurantsDropDown :selected').text())
   $("#bookedRestaurant").append('<span>' + $("#restaurantsDropDown :selected").text() +'</span>')
+  $('#bookedRestaurant').append('<button class="btn btn-xs btn-danger remove btn-circle pull-right">x</button>')
+
   var restaurantCoordinate;
   for (var i =0; i<restaurants.length; i++){
     if ( restaurants[i].name === $("#restaurantsDropDown :selected").text() ){
@@ -36,8 +40,9 @@ $("#restaurantAdd").on('click', function(){
 })
 
 $("#activityAdd").on('click', function(){
-  console.log($('#activitiesDropDown :selected').text())
   $("#bookedActivity").append('<span>' + $("#activitiesDropDown :selected").text() +'</span>')
+  $('#bookedActivity').append('<button class="btn btn-xs btn-danger remove btn-circle pull-right">x</button>')
+
   var activityCoordinate;
   for (var i =0; i<activities.length; i++){
     if ( activities[i].name === $("#activitiesDropDown :selected").text() ){
@@ -46,3 +51,13 @@ $("#activityAdd").on('click', function(){
   }
   drawMarker('activity', activityCoordinate)
 })
+
+// REMOVE ITINERARY ITEMS
+
+$('#bookedHotel').on('click', '.remove', function() {
+  $(this).prev().remove()
+  $(this).remove()
+});
+
+
+  // console.log($('#restaurantsDropDown :selected').text())
